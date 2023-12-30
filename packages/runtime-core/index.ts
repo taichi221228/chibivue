fd55e722ef3680e8e45e93cf317d181e98b76c30
taichi221228 <taichi221228@icloud.com>
2023-12-30 09:34:22 +0900

    Add basic interfaces for renderer in `runtime-core`
    Created RendererOptions, RenderNode, and RenderElement interfaces in `runtime-core/renderer.ts`. These will serve as the groundwork for defining core operational functions related to rendering. Currently, the interfaces are still under development.

---
+++ packages/runtime-core/index.ts
@@ -0,0 +1,1 @@
+export { createRenderer, type RendererOptions } from "./renderer";
---
+++ packages/runtime-core/renderer.ts
@@ -0,0 +1,28 @@
+export interface RendererOptions<HostNode = RenderNode> {
+  setElementText: (node: HostNode, text: string) => void;
+}
+
+export interface RenderNode {
+  // TODO: work in progress
+  [key: string]: any;
+}
+
+// TODO: work in progress
+export interface RenderElement extends RenderNode {
+}
+
+export type RootRenderFunction<HostElement = RenderElement> = (
+  message: string,
+  container: HostElement,
+) => void;
+
+export function createRenderer(options: RendererOptions) {
+  const { setElementText: hostElementText } = options;
+
+  const render: RootRenderFunction = (message, container) => {
+    // TODO: work in progress. This is just a placeholder.
+    hostElementText(container, message);
+  };
+
+  return { render };
+}
