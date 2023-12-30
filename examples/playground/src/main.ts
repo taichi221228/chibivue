efb672c60acd1239a398738a4dcf76715002f59c
taichi221228 <taichi221228@icloud.com>
2023-12-30 18:08:05 +0900

    Expand rendering method in createApp
    The rendering method in createApp within main.ts has been expanded to accommodate more complex HTML structures. Included now are nested elements and text nodes, improving flexibility and widening options during the rendering process.

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,7 +1,16 @@
 import { createApp, h } from "chibivue";
 
 const app = createApp({
-  render: () => h("div", {}, ["Hello Chibivue!"]),
+  render: () =>
+    h("div", {}, [
+      h("h1", {}, ["Hello, Chibivue!"]),
+      h("p", {}, ["This is nested paragraph."]),
+      h("div", {}, [
+        h("span", {}, ["This is nested span."]),
+        h("button", {}, ["Click me!"]),
+      ]),
+      "This is a text node.",
+    ]),
 });
 
 app.mount("#app");
