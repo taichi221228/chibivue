ab7936d3d2296aeb90548a5531484834741158b8
taichi221228 <taichi221228@icloud.com>
2023-12-30 10:47:54 +0900

    Replace runtime-dom export with runtime-core
    The runtime-dom index code has been significantly simplified by replacing the previously detailed export with an all-inclusive reference to "./runtime-core". This change helps streamline the runtime-dom package exports, makes for cleaner, more maintainable code, and enables easier future expansion.

--- packages/index.ts
+++ packages/index.ts
@@ -1,14 +1,1 @@
-export type Options = {
-  render: () => string;
-};
-
-export type App = {
-  mount: (selector: string) => void;
-};
-
-export const createApp = (options: Options): App => ({
-  mount: (selector: string) => {
-    const root = document.querySelector(selector);
-    if (root) root.innerHTML = options.render();
-  },
-});
+export * from "./runtime-dom";
