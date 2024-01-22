4984679ab7f9f3b275ae10feb6cf37ad114c2703
taichi221228 <taichi221228@icloud.com>
2024-01-22 17:02:33 +0900

    Refactor main.ts to use CounterComponent
    The main.ts codebase has been refactored to use a separate CounterComponent. This component encapsulates all logic and templating related to the counter functionality, improving the structure and readability of the code. The main application now includes multiple instances of the CounterComponent.

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,19 +1,25 @@
 import { createApp, h, reactive } from "chibivue";
 
-const app = createApp({
+const CounterComponent = {
   setup: () => {
     const state = reactive({ count: 0 });
     const increment = () => state.count++;
 
     return () =>
-      h("div", { id: "chibivue" }, [
-        h("h1", {}, [
-          "count: ",
-          h("span", {}, [state.count + ""]),
-        ]),
+      h("div", {}, [
+        h("p", {}, [`count: ${state.count}`]),
         h("button", { onClick: increment }, ["increment!"]),
       ]);
   },
+};
+
+const app = createApp({
+  setup: () => () =>
+    h("div", { id: "chibivue" }, [
+      h(CounterComponent, {}, []),
+      h(CounterComponent, {}, []),
+      h(CounterComponent, {}, []),
+    ]),
 });
 
 app.mount("#app");
