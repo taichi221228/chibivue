de4d553ff2a870cacc22157e937c7897a572aad5
taichi221228 <taichi221228@icloud.com>
2024-01-23 18:46:30 +0900

    Add prop initialization in component setup process
    Implemented a function in componentProps.ts for prop initialization in components during setup. This enhances the component setup by allowing the setup function to consider props as an argument. An additional helper function was also added, which not just initializes the props but also declares them as reactive.

--- packages/runtime-core/componentProps.ts
+++ packages/runtime-core/componentProps.ts
@@ -1,3 +1,5 @@
+import { type ComponentInternalInstance, type Data, reactive } from "chibivue";
+
 export type Props = Record<string, PropOptions | null>;
 
 export interface PropOptions<T = any> {
@@ -7,3 +9,26 @@ export interface PropOptions<T = any> {
 }
 
 export type PropType<T> = { new (...args: any[]): T & {} };
+
+export const initProps = (
+  instance: ComponentInternalInstance,
+  rawProps: Data | null,
+) => {
+  const props: Data = {};
+  setFullProps(instance, rawProps, props);
+  instance.props = reactive(props);
+};
+
+const setFullProps = (
+  instance: ComponentInternalInstance,
+  rawProps: Data | null,
+  props: Data,
+) => {
+  const options = instance.propsOptions;
+
+  if (rawProps) {
+    Object.entries(rawProps).forEach(([key, value]) => {
+      if (options && options.hasOwnProperty(key)) props[key] = value;
+    });
+  }
+};
