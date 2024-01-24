cdce65d2a6f2c46c8126825b0714cfbc2aa32719
taichi221228 <taichi221228@icloud.com>
2024-01-24 18:39:31 +0900

    Add 'hasOwn' and 'camelize' functions in general.ts
    The 'shared' module has been expanded with two new functions in the 'general.ts' file. The 'hasOwn' function determines if an object has a specific property and 'camelize' changes hyphen-separated strings to camelCase. These features are designed to provide shared utilities for the application.

--- packages/shared/general.ts
+++ packages/shared/general.ts
@@ -1,1 +1,9 @@
-export {}
+export const hasOwn = (
+  val: object,
+  key: string | symbol,
+): key is keyof typeof val => Object.prototype.hasOwnProperty.call(val, key);
+
+const camelizeRE = /-(\w)/g;
+
+export const camelize = (str: string): string =>
+  str.replace(camelizeRE, (_, c) => (c ? c.toUpperCase() : ""));
