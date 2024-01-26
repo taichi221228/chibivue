b4b9bd783c3deba25212a70e0eabc681b6ab40e7
taichi221228 <taichi221228@icloud.com>
2024-01-26 16:54:36 +0900

    Add empty componentEmits.ts file and export it in index.ts
    An empty file 'componentEmits.ts' has been created in the '/runtime-core' directory and it's also been exported in the main 'index.ts' file. The purpose of this file is not clear yet as the file is currently empty.

--- packages/runtime-core/index.ts
+++ packages/runtime-core/index.ts
@@ -1,6 +1,7 @@
 export * from "./apiCreateApp";
 export * from "./component";
 export * from "./componentOptions";
+export * from "./componentEmits";
 export * from "./componentProps";
 export * from "./h";
 export * from "./renderer";
