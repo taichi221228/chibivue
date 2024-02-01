f8116071ed9b3de001b561d4c9dace8f195e04bd
taichi221228 <taichi221228@icloud.com>
2024-02-01 18:52:14 +0900

    Export new AST constructs in compiler-core package
    This commit introduces several new AST constructs in the compiler-core package, which include NodeTypes, Position, SourceLocation, and Node interfaces, along with their respective extensions. Additionally, the placeholder 'ast.ts' file has been replaced with these new constructs, setting the foundation for more complex AST operations in the future.

--- packages/compiler-core/ast.ts
+++ packages/compiler-core/ast.ts
@@ -1,1 +1,42 @@
-export {};
+export const enum NodeTypes {
+  TEXT,
+  ELEMENT,
+  ATTRIBUTE,
+}
+
+export interface Position {
+  offset: number;
+  line: number;
+  column: number;
+}
+export interface SourceLocation {
+  start: Position;
+  end: Position;
+  source: string;
+}
+
+export interface Node {
+  type: NodeTypes;
+  loc: SourceLocation;
+}
+
+export interface TextNode extends Node {
+  type: NodeTypes.TEXT;
+  content: string;
+}
+
+export interface ElementNode extends Node {
+  type: NodeTypes.ELEMENT;
+  tag: string;
+  props: AttributeNode[];
+  children: TemplateChildNode[];
+  isSelfClosing: boolean;
+}
+
+export interface AttributeNode extends Node {
+  type: NodeTypes.ATTRIBUTE;
+  name: string;
+  value: TextNode | undefined;
+}
+
+export type TemplateChildNode = TextNode | ElementNode;
