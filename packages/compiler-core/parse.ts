96473f5f48b071a1a77a776f7b34512a0417bf9d
taichi221228 <taichi221228@icloud.com>
2024-02-04 12:15:00 +0900

    Implement parsing functionality in compiler-core
    This commit introduces helper functions parseText and parseTextData to the parse.ts module, which are responsible for parsing text content. Additionally, the code optimizes the baseParse function, which now returns an array of TemplateChildNode objects. These changes improve the overall parsing functionality provided by the compiler-core module.

--- packages/compiler-core/parse.ts
+++ packages/compiler-core/parse.ts
@@ -1,4 +1,11 @@
-import { type ElementNode, NodeTypes, type TemplateChildNode } from "chibivue";
+import {
+  type ElementNode,
+  NodeTypes,
+  type Position,
+  type SourceLocation,
+  type TemplateChildNode,
+  type TextNode,
+} from "chibivue";
 
 export interface ParserContext {
   readonly originalSource: string;
@@ -16,7 +23,27 @@ const createParserContext = (content: string): ParserContext => ({
   column: 1,
 });
 
-// TODO: const parseText = (context: ParserContext): TextNode => {};
+const parseText = (context: ParserContext): TextNode => {
+  const start = getCursor(context);
+  const endToken = "<";
+  const index = context.source.indexOf(endToken, 1);
+
+  let endIndex = context.source.length;
+  if (index !== -1 && endIndex > index) endIndex = index;
+
+  const content = parseTextData(context, endIndex);
+
+  return {
+    type: NodeTypes.TEXT,
+    content,
+    loc: getSelection(context, start),
+  };
+};
+
+const parseTextData = (context: ParserContext, length: number): string => {
+  advancedBy(context, length);
+  return context.source.slice(0, length);
+};
 
 // TODO: const parseElement = (context: ParserContext, ancestors: ElementNode[]): ElementNode | undefined => {};
 
@@ -36,9 +63,7 @@ const parseChildren = (
       }
     } */
 
-    /* TODO: if (!node) {
-      node = parseText(context);
-    } */
+    if (!node) node = parseText(context);
 
     // TODO: pushNode(nodes, node);
   }
@@ -77,6 +102,55 @@ const pushNode = (
   }
 
   nodes.push(node);
+};
+
+const advancedBy = (
+  context: ParserContext,
+  numberOfCharacters: number,
+): void => {
+  const { source } = context;
+};
+const advancePositionWithMutation = (
+  position: Position,
+  source: string,
+  numberOfCharacters: number,
+): Position => {
+  const NEW_LINE_CHAR_CODE = 10;
+  let linesCount = 0;
+  let lastNewLinePosition = -1;
+
+  for (let i = 0; i < numberOfCharacters; i++) {
+    if (source.charCodeAt(i) === NEW_LINE_CHAR_CODE) {
+      linesCount++;
+      lastNewLinePosition = i;
+    }
+  }
+
+  position.offset += numberOfCharacters;
+  position.line += linesCount;
+  position.column = lastNewLinePosition === -1
+    ? position.column + numberOfCharacters
+    : numberOfCharacters - lastNewLinePosition;
+
+  return position;
+};
+
+const getCursor = ({ offset, line, column }: ParserContext): Position => ({
+  offset,
+  line,
+  column,
+});
+const getSelection = (
+  context: ParserContext,
+  start: Position,
+  end?: Position,
+): SourceLocation => {
+  end = end ?? getCursor(context);
+  return {
+    start,
+    end,
+    source: context.originalSource.slice(start.offset, end.offset),
+  };
 };
 
 const last = <T>(xs: T[]): T | undefined => xs[xs.length - 1];
