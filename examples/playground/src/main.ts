e7ba4d21bf63343bdf55ef3f2b810993a97de3c7
taichi221228 <taichi221228@icloud.com>
2024-02-10 18:04:46 +0900

    Update UI enhancement in main.ts
    Improved the UI's visual appeal by adjusting font sizes, enlarging the logo size, and updating UI styling properties. Also, the background color and font have been modified to "whitesmoke" and "dimgray" to enhance the visual aesthetics.

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -3,20 +3,35 @@ import { createApp } from "chibivue";
 const app = createApp({
   template: `<div
     class="container"
-    style="display: flex;flex-direction: column;justify-content: center;align-items: center;gap: 48px;"
   >
-    <h1>Hello, Chibivue!</h1>
+    <h1 style="font-size: 60px;">Hello, Chibivue!</h1>
     <img
       src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png"
       alt="Vue.js Logo"
-      width="150"
+      width="260"
     />
-    <p>
+    <p style="font-size: 20px;">
       <b>Chibivue</b> is the minimal Vue.js
     </p>
     <style>
-      .container {
+      body {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
         height: 100svh;
+      }
+      
+      #app {
+        height: 100%;
+      }
+      
+      .container {
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+        align-items:center;
+        gap: 48px;
+        box-sizing: border-box;
+        height: 100%;
         padding: 16px;
         background-color: whitesmoke;
         color: dimgray;
