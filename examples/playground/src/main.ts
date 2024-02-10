3e63e3fe7de5ca73c433eb749477d541a8c99910
taichi221228 <taichi221228@icloud.com>
2024-02-10 17:40:26 +0900

    Update main template in ChibiVue app
    Expanded the main template in the Chibivue playground application. This involves formatting changes for code readability, and includes the addition of new HTML elements for better visual representation and demonstration of the framework's capabilities.

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,7 +1,34 @@
 import { createApp } from "chibivue";
 
 const app = createApp({
-  template: `<h1>Hello ChibiVue!</h1>`,
+  template: `
+    <div
+      class="container"
+      style="
+        display: flex;
+        flex-direction: column;
+        justify-content:center;
+      "
+    >
+      <h1>Hello, Chibivue!</h1>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png"
+        alt="Vue.js Logo"
+        width="150"
+      />
+      <p>
+        <b>Chibivue</b> is the minimal Vue.js
+      </p>
+      <style>
+        .container {
+          height: 100svh;
+          padding: 16px;
+          background-color: #becdbe;
+          color: #2c3e50;
+        }
+      </style>
+    </div>
+  `,
 });
 
 app.mount("#app");
