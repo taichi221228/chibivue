ae8b44bd657719c5f47dc9b311d92f3913fe6aa2
taichi221228 <taichi221228@icloud.com>
2024-02-10 18:23:45 +0900

    Adjust "setup" function's return type in componentOptions.ts
    Modified the "setup" function within the componentOptions file to allow it to return either a Function or void. This change provides more flexibility by accommodating situations where no functional return is required.

--- packages/runtime-core/componentOptions.ts
+++ packages/runtime-core/componentOptions.ts
@@ -3,7 +3,7 @@ export interface ComponentOptions {
   setup?: (
     props: ComponentOptions["props"],
     ctx: { emit: (event: string, ...args: any[]) => void },
-  ) => Function;
+  ) => Function | void;
   render?: Function;
   template?: string;
 }
