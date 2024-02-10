8e96d98e634038a0aeb3b8f5ffb4110d4707518b
taichi221228 <taichi221228@icloud.com>
2024-02-10 16:18:03 +0900

    Update 'generate' function in compiler-core package
    The 'generate' function was updated in the compiler-core package to handle different types of nodes. The update introduced 'genNode', 'genText', and 'genElement' functions. Now, the processing of Text and Element nodes is performed more efficiently.

--- packages/compiler-core/codegen.ts
+++ packages/compiler-core/codegen.ts
@@ -1,17 +1,42 @@
-export const generate = ({
-  tag,
-  props,
-  textContent,
-}: {
-  tag: string;
-  props: Record<string, string>;
-  textContent: string;
-}): string =>
-  `return () => {
+import {
+  type ElementNode,
+  NodeTypes,
+  type TemplateChildNode,
+  type TextNode,
+} from "chibivue";
+
+export const generate = (
+  { children }: { children: TemplateChildNode[] },
+): string => {
+  const [child] = children;
+  return `return () => {
     const { h } = _chibivue;
-    return h(
-      "${tag}",
-      { ${Object.entries(props).map(([k, v]) => `${k}: "${v}"`).join(", ")} }, 
-      ["${textContent}"]
-    );
+    return ${genNode(child)};
   };`;
+};
+
+const genNode = (node: TemplateChildNode): string => {
+  switch (node.type) {
+    case NodeTypes.TEXT:
+      return genText(node);
+    case NodeTypes.ELEMENT:
+      return genElement(node);
+    default:
+      return "";
+  }
+};
+
+const genText = (text: TextNode): string => `\`${text.content}\``;
+
+const genElement = ({ tag, ...element }: ElementNode): string => {
+  const props = element.props.map(({ name, value }) =>
+    `${name}: "${value?.content}"`
+  ).join(", ");
+  const children = element.children.map((it) => genNode(it)).join(", ");
+
+  return `h(
+    "${tag}",
+    { ${props} },
+    [${children}]
+  );`;
+};
