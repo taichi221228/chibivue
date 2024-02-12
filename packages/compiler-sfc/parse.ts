5caa3f56f6aec3b503c89e81a64dfb447d2c855d
taichi221228 <taichi221228@icloud.com>
2024-02-12 14:58:04 +0900

    Implement structures for SFC compiler and parse function
    Introduced interfaces for different SFC blocks as part of the initial setup for the SFC compiler and parsing functionality. Also, these interfaces are integrated into the compiler-sfc files to lay the groundwork for future development.

--- packages/compiler-sfc/parse.ts
+++ packages/compiler-sfc/parse.ts
@@ -1,1 +1,28 @@
-// TODO: Implement the parse function
+import { type SourceLocation } from "chibivue";
+
+export interface SFCDescriptor {
+  id: string;
+  filename: string;
+  source: string;
+  template: SFCTemplateBlock | null;
+  script: SFCScriptBlock | null;
+  styles: SFCStyleBlock[];
+}
+
+export interface SFCBlock {
+  type: string;
+  content: string;
+  location: SourceLocation;
+}
+
+export interface SFCTemplateBlock extends SFCBlock {
+  type: "template";
+}
+
+export interface SFCScriptBlock extends SFCBlock {
+  type: "script";
+}
+
+export interface SFCStyleBlock extends SFCBlock {
+  type: "style";
+}
