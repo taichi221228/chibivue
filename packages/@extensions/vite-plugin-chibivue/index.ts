ecf77ce3c96fd1062730276418cd51916da49229
taichi221228 <taichi221228@icloud.com>
2024-02-12 14:50:26 +0900

    Implement .vue file filter in chibivue plugin
    The chibivue plugin has been modified to incorporate a filter specific to .vue files. This change ensures that the transform function of this plugin is only applied to .vue files, improving efficiency in the Vite build process and enhancing Vue's operational functionality.

--- packages/@extensions/vite-plugin-chibivue/index.ts
+++ packages/@extensions/vite-plugin-chibivue/index.ts
@@ -1,6 +1,16 @@
-import { type Plugin } from "vite";
+import { createFilter, type Plugin } from "vite";
 
-export default (): Plugin => ({
-  name: "vite:chibivue",
-  transform: (code, _id) => ({ code }),
-});
+export default (): Plugin => {
+  const filter = createFilter(/\.vue$/);
+
+  return {
+    name: "vite:chibivue",
+    transform: (code, id) => {
+      if (!filter(id)) return;
+      return {
+        // TODO: Implement the transform function
+        code: "export default {}",
+      };
+    },
+  };
+};
