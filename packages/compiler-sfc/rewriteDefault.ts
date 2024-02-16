13d1f57e66445e308d88dec9285d79e98acb47b2
taichi221228 <taichi221228@icloud.com>
2024-02-16 16:24:55 +0900

    Implement `rewriteDefault` function template in `compiler-sfc`
    The template for a new function `rewriteDefault` has been put in place within `rewriteDefault.ts` in the `compiler-sfc` package. This preliminary setup includes regular expressions to identify default and named default exports, hinting at functionality to manipulate such exports. The actual implementation is pending.

--- packages/compiler-sfc/rewriteDefault.ts
+++ packages/compiler-sfc/rewriteDefault.ts
@@ -1,1 +1,7 @@
-export {}
+const defaultExportRE = /((?:^|\n|;)\s*)export\s+default\s+/;
+const namedDefaultExportRE = /((?:^|\n|;)\s*)export(.+)(?:as)?(\s*)default/s;
+
+export const rewriteDefault = (input: string, as: string): string => {
+  // TODO: Impl
+  return "";
+};
