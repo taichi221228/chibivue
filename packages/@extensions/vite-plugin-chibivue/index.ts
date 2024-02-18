f07948f83cf14952ab9822cb8456d9af7e548c6a
taichi221228 <taichi221228@icloud.com>
2024-02-18 15:41:30 +0900

    Add CSS support to vite-plugin-chibivue
    Updated the vite-plugin-chibivue to handle '.vue.css' files. Added 'fs' import for file operations and created new methods for loading and resolving CSS files in Vue components.

--- packages/@extensions/vite-plugin-chibivue/index.ts
+++ packages/@extensions/vite-plugin-chibivue/index.ts
@@ -1,3 +1,4 @@
+import fs from "node:fs";
 import { createFilter, type Plugin } from "vite";
 
 import { compile } from "../../compiler-dom";
@@ -8,6 +9,7 @@ export default (): Plugin => {
 
   return {
     name: "vite:chibivue",
+
     transform: (code, id) => {
       if (!filter(id)) return;
 
@@ -25,10 +27,29 @@ export default (): Plugin => {
 
       return {
         code: `import * as _chibivue from "chibivue";
+          import "${id}.css";
+
           ${scriptCode}
           ${templateCode}
+
           export default { ...${SFC_MAIN}, render };`,
       };
+    },
+
+    load: (id) => {
+      if (id.match(/\.vue\.css$/)) {
+        const fileName = id.replace(/\.css$/, "");
+        const content = fs.readFileSync(fileName, "utf-8");
+        const { descriptor } = parse(content, { fileName });
+
+        return {
+          code: descriptor.styles.map((style) => style.content).join("\n"),
+        };
+      }
+    },
+
+    resolveId: (id) => {
+      if (id.match(/\.vue\.css$/)) return id;
     },
   };
 };
