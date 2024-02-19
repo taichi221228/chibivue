1aa0d6dfbd49f6e721401ea722b138b82fcc0ddb
taichi221228 <taichi221228@icloud.com>
2024-02-19 15:19:12 +0900

    Refactor playground's main.ts to use reactive list
    Replaced the previous Vue application initialization in main.ts of the playground example, with a more complex example that uses a reactive list. This example includes a button that updates the reactive list, demonstrating more of the ChibiVue functionality.

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,8 +1,24 @@
-import { createApp } from "chibivue";
+import { createApp, h, reactive } from "chibivue";
 
-// @ts-expect-error
-import App from "./App.vue";
+const app = createApp({
+  setup() {
+    const state = reactive({
+      list: [{ key: "a" }, { key: "b" }, { key: "c" }, { key: "d" }],
+    });
+    const updateList = () => {
+      state.list = [{ key: "a" }, { key: "b" }, { key: "d" }, { key: "c" }];
+    };
 
-const app = createApp(App);
+    return () =>
+      h("div", { id: "app" }, [
+        h(
+          "ul",
+          {},
+          state.list.map((item) => h("li", { key: item.key }, [item.key])),
+        ),
+        h("button", { onClick: updateList }, ["update"]),
+      ]);
+  },
+});
 
 app.mount("#app");
