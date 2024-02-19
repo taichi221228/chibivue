290fd26922cde127e50517dac544e9a26b83458e
taichi221228 <taichi221228@icloud.com>
2024-02-19 11:32:33 +0900

    Refactor baseCompile function in compiler-core/compile.ts
    Simplified baseCompile function in compiler-core/compile.ts by removing the parseResult constant. The baseParse function now directly feeds into the generate function, aiming to increase code size efficiency and readability.

--- packages/compiler-core/compile.ts
+++ packages/compiler-core/compile.ts
@@ -3,8 +3,4 @@ import { baseParse, type CompilerOptions, generate } from "../compiler-core";
 export const baseCompile = (
   template: string,
   options: Required<CompilerOptions>,
-) => {
-  const parseResult = baseParse(template.trim());
-
-  return generate(parseResult, options);
-};
+) => generate(baseParse(template.trim()), options);
